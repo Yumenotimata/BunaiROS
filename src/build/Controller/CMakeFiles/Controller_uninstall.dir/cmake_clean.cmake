c575f4f6331020c88a260447f37fb9bdbe0b871f
Yumenotimata <kouyayamaoka@gmail.com>
2023-04-06 20:20:35 +0900

    first commit

---
+++ src/build/Controller/CMakeFiles/Controller_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Controller_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Controller_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
